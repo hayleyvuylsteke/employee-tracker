63d31b760bd7227cfb6afa594569ea4086fa7789
hayleyvuylsteke <72625026+hayleyvuylsteke@users.noreply.github.com>
2021-02-24 20:57:57 -0500

    updates to frontend

--- app.js
+++ app.js
@@ -17,10 +17,38 @@ initiate = () => {
         type: 'list',
         name: 'options',
         message: 'What can I help you with today?',
-        choices: ['']
+        choices: ['View all employees.', 'View all Employees by Department.', 'View all Employees by Manager.', 'Add an employee.', 'Update an employee.', 'Delete an employee.', ]
         }
 
-    ])
+    ]).then(function(option){
+        if(option.choice === "View all employees." || "View all Employees by Department." ||"View all Employees by Manager." ) {
+            viewEmployee(option.choice)
+        }
+        else if (option.choice === "Add an employee.") {
+            addEmployee();
+        }
+        else if (option.choice === "Update an employee.") {
+            updateEmployee();
+        }
+        else if (option.choice === "Delete an employee.") {
+            deleteEmployee();
+        }
+    })
     
 }
 
+viewEmployee = (option) => {
+    //add view functions
+}
+
+addEmployee = () => {
+    //add view functions
+}
+
+updateEmployee = () => {
+    //add update functions
+}
+
+deleteEmployee = () => {
+    //add delete functions
+}
